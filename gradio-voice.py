f4c65e24fc5aae9c14a79feeaca63bbe45cd1ce8
Susinder Gulasekaran <susinderrajan@gmail.com>
2023-05-16 10:46:13 -0700

    Added support for voice input in tamil language with automatic language detection. Had to increase whisper model size to get better tamil word error rate. Use deep_translator instead of whisper's translation which was poor. Verified in windows os.

--- gradio-voice.py
+++ gradio-voice.py
@@ -3,21 +3,21 @@ import whisper
 import gradio as gr
 from langchain.llms import OpenAI
 from langchain.agents import load_tools, initialize_agent, AgentType
+from deep_translator import GoogleTranslator
+
 
 # Import the required module for text
 # to speech conversion
 from AppKit import NSSpeechSynthesizer
 
 nssp = NSSpeechSynthesizer
-
 ve = nssp.alloc().init()
 
 # import the openAI
 llm = OpenAI(temperature=0.9)
 
 # to get input from speech use the following libs
-p = pipeline("automatic-speech-recognition")
-model = whisper.load_model("base")
+model = whisper.load_model("medium")
 
 # create a list of tools
 tool_names = [
@@ -32,7 +32,6 @@ agent = initialize_agent(
     tools=tools, llm=llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
 )
 
-
 # core function which will do all the work (POC level code)
 def transcribe(audio, state=""):
     # load audio and pad/trim it to fit 30 seconds
@@ -44,15 +43,35 @@ def transcribe(audio, state=""):
 
     # detect the spoken language
     _, probs = model.detect_language(mel)
+    detected_language = max(probs, key=probs.get)
+    if detected_language == "en":
+        print("Detected English language.")
+        options = whisper.DecodingOptions(fp16=False, task="transcribe", language="en")
+        result = whisper.decode(model, mel, options)
+        result_text = result.text
+    elif detected_language == "ta":
+        print("Detected Tamil language.")
+        options = whisper.DecodingOptions(fp16=False, task="transcribe", language="ta")
+        tamil = whisper.decode(model, mel, options)
+        print(tamil.text)
+        result_text = GoogleTranslator(source='ta', target='en').translate(tamil.text)
 
-    # decode the audio
-    options = whisper.DecodingOptions(fp16=False)
-    result = whisper.decode(model, mel, options)
-    result_text = result.text
+        # transcribe = pipeline(task="automatic-speech-recognition", model="vasista22/whisper-tamil-medium",
+        #                       chunk_length_s=30, device="cpu")
+        # transcribe.model.config.forced_decoder_ids = transcribe.tokenizer.get_decoder_prompt_ids(language="ta", task="transcribe")
+        # print('Transcription: ', transcribe(audio)["text"])
+        # result_text = "Unknown language"
+    else:
+        result_text = "Unknown language"
+
     print("result text --> ", result_text)
-    # Now add the lanfChain logic here to process the text and get the responses.
-    # once we get the response, we can output it to the voice.
-    output = agent.run(result_text)
+    if result_text != "Unknown language":
+        # Now add the lanfChain logic here to process the text and get the responses.
+        # once we get the response, we can output it to the voice.
+        output = agent.run(result_text)
+    else:
+        output = "I'm sorry I cannot understand the language you are speaking. Please speak in English or Tamil."
+
     # # say method on the engine that passing input text to be spoken
     ve.startSpeakingString_(output)
 
