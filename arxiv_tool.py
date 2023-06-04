c54ad408250ec43286ad3db6d1d904cdab9e4d77
Kashish Pandey <56780262+kashishpandey@users.noreply.github.com>
2023-06-03 17:07:32 -0700

    arxiv tool

---
+++ arxiv_tool.py
@@ -0,0 +1,16 @@
+from langchain import ArxivAPIWrapper
+import arxiv as arxiv_module
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import load_tools, initialize_agent, AgentType
+from langchain.tools import Tool
+from pydantic import BaseModel, Field
+arxiv_wrapper = ArxivAPIWrapper()
+class ArxivInput(BaseModel):
+    preprint: str = Field()
+
+arxiv_doc_tool = Tool.from_function(
+    name="arxiv",
+    func=arxiv_wrapper.run,
+    args_schema=ArxivInput,
+    description="Search the arxiv pre-print repository",
+)
